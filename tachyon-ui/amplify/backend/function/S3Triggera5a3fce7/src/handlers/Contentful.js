766bce27ad789483dbaebc51f026af975855f137
Brian Winkers <bwinkers@gmail.com>
2021-02-28 11:06:09 -0800

    WiP

--- tachyon-ui/amplify/backend/function/S3Triggera5a3fce7/src/handlers/Contentful.js
+++ tachyon-ui/amplify/backend/function/S3Triggera5a3fce7/src/handlers/Contentful.js
@@ -1,9 +1,31 @@
+/* Amplify Params - DO NOT EDIT
+    API_TACHYONAPI_GRAPHQLAPIENDPOINTOUTPUT
+    API_TACHYONAPI_GRAPHQLAPIIDOUTPUT
+    ENV
+    REGION
+Amplify Params - DO NOT EDIT */// eslint-disable-next-line
+
 'use strict'
 
-const oboe = require('oboe')
+const AWS = require('aws-sdk')
+const S3 = new AWS.S3({ signatureVersion: 'v4' })
+const AUTH_TYPE = require('aws-appsync').AUTH_TYPE
+const AWSAppSyncClient = require('aws-appsync').default
+const uuidv4 = require('uuid/v4')
+const gql = require('graphql-tag')
 
-var typesCount = 0;
-var tagsCount = 0;
-var assetsCount = 0;
-var localesCount = 0;
-var entriesCount = 0;
+const oboe = require('oboe') 
+
+class ContentfulImporter {
+    constructor() {
+
+    }
+
+    // Process an S3 record representing an uploaded Contentful export  
+    async processRecord(record) {
+
+        console.log('Processing Contentful Import.')
+    }
+}
+
+export default new ContentfulImporter()
